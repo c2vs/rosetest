961a6443ee13cf669ab83c1d48a2d1a13f6e93a5
c2vs <104372250+c2vs@users.noreply.github.com>
2022-06-08 23:54:10 +0300

    Update disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -2,13 +2,13 @@ const Command = require("../structures/command.js");
 
 module.exports = new Command({
 	name: "disconnect",
-	aliases: ['dc'],
-	description: "Disconnects from the music channel and clears the server queue",
+	aliases: ['dc', 'انجبي'],
+	description: "تطفي الاغاني وتطلع من الروم",
 	permission: "SEND_MESSAGES",
 	async run(message, args, client, slash) {
         const queue = client.player.getQueue(message.guild);
         if (queue) await queue.destroy(true);
 		message.guild.me.voice.disconnect();
-        slash ? message.reply({embeds: [{ description: `👋 Disconnected.`, color: 0x44b868 }]}) : message.react('👋');
+        slash ? message.reply({embeds: [{ description: `👋 طفيته, color: 0x44b868 }]}) : message.react('👋');
 	}
 });
